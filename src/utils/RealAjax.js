a783d79f4d964e271dbbdfd452dc5614f39170c1
super-ai <294473456@qq.com>
2017-08-29 00:05:16 +0800

    删除bug 消除

--- src/utils/RealAjax.js
+++ src/utils/RealAjax.js
@@ -70,9 +70,9 @@ class Ajax {
     });
   }
 
-  get(url, params = {}) {
-    return this.requestWrapper('GET', url, {params});
-    // return this.requestWrapper('GET', url, {...opts});
+  get(url, opts = {}) {
+    // return this.requestWrapper('GET', url, {params});
+    return this.requestWrapper('GET', url, {...opts});
   }
 
   //data body传递 ; opts params接收
