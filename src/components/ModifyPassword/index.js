3340bd301d29def0b0563bddccd9894a1c40ccaf
super-ai <294473456@qq.com>
2017-09-07 19:09:42 +0800

    能正确返回错误信息

--- src/components/ModifyPassword/index.js
+++ src/components/ModifyPassword/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Form,Input,Button } from 'antd';
+import { Form,Input,Button,notification } from 'antd';
 import './index.less';
+import ajax from 'utils/ajax.js';
 
 const Component = React.Component;
 const FormItem = Form.Item;
-
 
 class ModifyPassword extends Component{
   state = {
@@ -36,15 +36,43 @@ class ModifyPassword extends Component{
 
   /**
   * 提交密码修改
+  * 不能使用箭头函数
   */
-  handleSubmit = (e) => {
+  async handleSubmit(e){
    e.preventDefault();
    this.props.form.validateFieldsAndScroll((err, values) => {
-     if (!err) {
-       // 提交数据
-       console.log('提交的表单数据为: ', values);
+     if (err) {
+       return;
      }
    });
+
+   try{ // 提交数据
+     console.log('提交的表单数据为: ',this.props.form.getFieldsValue());
+
+     const res = await ajax.get('/api/staff/password',this.props.form.getFieldsValue());
+
+     if (res.success) {
+       notification.success({
+         message: '修改密码成功',
+        //  description: this.primaryKey ? `新增数据行 主键=${res.data[this.primaryKey]}` : '',
+         duration: 3,
+       });
+       this.props.form.resetFields();
+     } else {
+       this.error(res.failInfo.errorMessage);
+     }
+   }catch(ex){
+     this.error(`网络请求出错: ${ex.message}`);
+   }
+  }
+
+  error(errorMsg) {
+    // 对于错误信息, 要很明显的提示用户, 这个通知框要用户手动关闭
+    notification.error({
+      message: '出错!',
+      description: `请联系管理员, 错误信息: ${errorMsg}`,
+      duration: 0,
+    });
   }
 
   render(){
@@ -77,7 +105,7 @@ class ModifyPassword extends Component{
       },
     };
     return(
-      <Form onSubmit={this.handleSubmit}>
+      <Form onSubmit={this.handleSubmit.bind(this)}>
         <FormItem label="旧密码" hasFeedBack {...formItemLayout}>
           {
             getFieldDecorator('oldPassword',{
