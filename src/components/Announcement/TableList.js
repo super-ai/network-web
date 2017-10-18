93d356b43db2e4a92edbb0e74123bcfab56aaab1
super-ai <294473456@qq.com>
2017-10-18 17:27:08 +0800

    寻找一种如何存储state参数的方法

--- src/components/Announcement/TableList.js
+++ src/components/Announcement/TableList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Button} from 'antd';
+import {Button,Table} from 'antd';
 
 const Component = React.Component;
 
@@ -9,15 +9,67 @@ class TableList extends Component{
     this.props.changeActiveComp('Detail');
   }
 
+  handleOnRowClick(record,index,event){
+    this.props.changeActiveComp('Detail');
+  }
+
   render(){
     return(
         <div>
-          <span>列表信息</span>
-          <br />
+          <Button>新建</Button>
+          <Button>检索</Button>
           <Button onClick={this.handleOnClick.bind(this)}>查看详细</Button>
+          <br />
+          <Table dataSource={data} columns={columns} onRowClick={this.handleOnRowClick.bind(this)}/>
         </div>
     )
   }
 }
 
 export default TableList;
+
+const columns = [{
+  title: '内容',
+  dataIndex: 'content',
+  key: 'content',
+}, {
+  title: '发布人',
+  dataIndex: 'createStaffName',
+  key: 'createStaffName',
+}, {
+  title: '发布时间',
+  dataIndex: 'createDateTime',
+  key: 'createDateTime',
+}];
+
+const data = [{
+  key: '1',
+  content: '长江流域发现白鱀豚',
+  createStaffName: '张三',
+  createDateTime: '2017-10-10 09:00:00',
+}, {
+  key: '2',
+  content: '共产党十九大胜利召开',
+  createStaffName: '李四',
+  createDateTime: '2016-10-10 09:00:00',
+}, {
+  key: '3',
+  content: '流氓斗殴此起彼伏',
+  createStaffName: '王五',
+  createDateTime: '2015-10-10 09:00:00',
+},{
+  key: '4',
+  content: '长江流域发现白鱀豚',
+  createStaffName: '张三',
+  createDateTime: '2017-10-10 09:00:00',
+}, {
+  key: '5',
+  content: '共产党十九大胜利召开',
+  createStaffName: '李四',
+  createDateTime: '2016-10-10 09:00:00',
+}, {
+  key: '6',
+  content: '流氓斗殴此起彼伏',
+  createStaffName: '王五',
+  createDateTime: '2015-10-10 09:00:00',
+}];
