deb62a6f67699e47cad84aa5dca268afb42a2caf
super-ai <294473456@qq.com>
2017-10-20 14:26:30 +0800

    回复做成树形结构

--- src/components/Announcement/detail.js
+++ src/components/Announcement/detail.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {Button,Form,Input,Icon} from 'antd';
+import {Button,Form,Input,Icon,Tree} from 'antd';
 import './index.less';
 
 const Component = React.Component;
 const FormItem = Form.Item;
 const {TextArea} = Input;
+const TreeNode = Tree.TreeNode;
 
 /**
 * 单条公告查看和编辑
@@ -44,8 +45,26 @@ class Detail extends Component{
 
   }
 
+  renderTreeNodes = (data) => {
+    return data.map((item) => {
+      if (item.children) {
+        return (
+          <TreeNode title={item.title} key={item.key} dataRef={item}>
+            {this.renderTreeNodes(item.children)}
+          </TreeNode>
+        );
+      }
+      return <TreeNode {...item} />;
+    });
+  }
+
+
   render(){
     const { getFieldDecorator } = this.props.form;
+    const replyTree =
+      <Tree>
+        {this.renderTreeNodes(replyTreeData)}
+      </Tree>;
     return(
       <div>
         <Button type='primary' icon='left-circle-o' onClick={this.handleReturn.bind(this)}>返回</Button>
@@ -54,7 +73,7 @@ class Detail extends Component{
         <Button icon='delete' onClick={this.handleDelete.bind(this)}>删除</Button>
         <Button icon='share-alt' onClick={this.handleTransmit.bind(this)}>转发</Button>
         <div>
-          <Form >
+          <Form>
             <FormItem label='标题' >
               {getFieldDecorator('title',{initialValue:this.state.selectedRow ? this.state.selectedRow.title:null})
                 (<Input />)}
@@ -67,6 +86,18 @@ class Detail extends Component{
               {getFieldDecorator('range',{initialValue:this.state.selectedRow ? this.state.selectedRow.range:null})
                 (<TextArea autosize={{ minRows: 6, maxRows: 6 }} />)}
             </FormItem>
+            <FormItem label='附件' >
+              {getFieldDecorator('attachments',{initialValue:this.state.selectedRow ? this.state.selectedRow.attachments:null})
+                (attachments)}
+            </FormItem>
+            <FormItem label='' >
+              {getFieldDecorator('replies',{initialValue:this.state.selectedRow ? this.state.selectedRow.replies:null})
+                (replyTree)}
+            </FormItem>
+            <FormItem label='已阅' >
+              {getFieldDecorator('reads',{initialValue:this.state.selectedRow ? this.state.selectedRow.reads:null})
+                (reads)}
+            </FormItem>
             <FormItem label='创建人' >
               {getFieldDecorator('createStaffName',{initialValue:this.state.selectedRow ? this.state.selectedRow.createStaffName:null})
                 (<Input />)}
@@ -75,10 +106,7 @@ class Detail extends Component{
               {getFieldDecorator('createDateTime',{initialValue:this.state.selectedRow ? this.state.selectedRow.createDateTime:null})
                 (<Input />)}
             </FormItem>
-            <FormItem label='附件' >
-              {getFieldDecorator('attachments',{initialValue:this.state.selectedRow ? this.state.selectedRow.attachments:null})
-                (<TextArea autosize={{ minRows: 4, maxRows: 4 }} />)}
-            </FormItem>
+
           </Form>
         </div>
       </div>
@@ -89,6 +117,53 @@ class Detail extends Component{
 const DetailForm = Form.create()(Detail);
 
 export default DetailForm;
+
+const attachments =
+  <ul>
+    <li>附件一</li>
+    <li>附件二</li>
+    <li>附件三</li>
+    <li>附件四</li>
+    <li>附件五</li>
+  </ul>;
+
+const reads =
+  <ul>
+    <li>张三阅</li>
+    <li>李四阅</li>
+    <li>Tom阅</li>
+    <li>Tyson阅</li>
+    <li>习大大阅</li>
+  </ul>;
+const replies =
+  <ul>
+    <li>张三回复</li>
+    <li>李四回复</li>
+    <li>Tom回复</li>
+    <li>Tyson回复</li>
+    <li>习大大回复</li>
+  </ul>;
+
+const replyTreeData = [{
+  title: '回复列表',
+  key: '0-0',
+  children: [{
+    title: '张三回复',
+    key: '0-0-0',
+  }, {
+    title: '李四回复',
+    key: '0-0-1',
+  }, {
+    title: 'Tom回复',
+    key: '0-0-2',
+  }, {
+    title: 'Tyson回复',
+    key: '0-1',
+  }, {
+    title: '习大大回复',
+    key: '0-2',
+  }],
+}];
 
 const formItemLayout = {
   labelCol: {
